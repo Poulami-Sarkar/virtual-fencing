9f8d3f78ab57dfccda477dd43ff488b94c374a36
Poulami Sarkar <poulamisarkar@SSMacBookPro2018.local>
2020-05-18 12:32:32 +0530

    GPS tracking

--- messaging/index.js
+++ messaging/index.js
@@ -11,17 +11,33 @@ const authToken = '65fc00134736043d3c09ef65ca53223f';
 const client = require('twilio')(accountSid, authToken);
 
 // Receiving a message
+var bounds=[];
 router.post('/', async (req, res) => {
     const { body } = req;
     let message;
-    console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
-    if (body.NumMedia > 0) {
-      message = new MessagingResponse().message("Thanks for the image! Here's one for you!");
-      //message.media(goodBoyUrl);
-    } else {
-      message = new MessagingResponse().message('Send us an image!');
+    var msg = req.body.Body;
+    var result;
+    console.log(`Incoming message from ${req.body.From}: ${msg}`);
+    if (msg){
+      email = msg.match(/^Email: (.*@*.)/);
+      bound = msg.match(/^Bounds: (.*)/);
+    } 
+    if(email && email.length > 1) {
+        message = new MessagingResponse().message("https://9e3a7839.ngrok.io?no="+req.body.From.toString().slice(10)+"&email="+email[1]);
+      } 
+    else if(bound){
+      var obj = JSON.parse(bound[1]);
+      if (obj.length >2){
+        bounds.push(bound[1])
+        message = new MessagingResponse().message('coordinates recieved: '+ bound[1]);
+      }
+      else
+        message = new MessagingResponse().message('Bounds format Bounds: [{"lat":123,"lng":213},{"lat":23,"lng":13},{"lat":12,"lng":2}] \n Specify minimum 3 coordinates');
     }
-  
+    else {
+        message = new MessagingResponse().message("https://9e3a7839.ngrok.io?no="+req.body.From.toString().slice(10));
+      }
+    
     res.set('Content-Type', 'text/xml');
     res.send(message.toString()).status(200);
   });
@@ -39,3 +55,4 @@ function send_whatsapp(msg,number){
 
 exports.router = router;
 exports.send_whatsapp = send_whatsapp;
+exports.bounds = bounds;
--- server.js
+++ server.js
@@ -4,6 +4,20 @@ var express = require('express');
 var fs = require('fs');
 const { urlencoded } = require('body-parser');
 var message = require('messaging')
+var nodemailer = require('nodemailer');
+
+// Node mailer
+let transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: 'poulamirulz@gmail.com',
+    pass: 'Rollercoaster47'
+  }
+});
+
 
 var app = express();
 app.use(urlencoded({ extended: false }));
@@ -22,3 +36,48 @@ app.get('/',function(req,resp){
 
 app.listen(8088);
 console.log('Server running at http://127.0.0.1:8088/');
+
+/*
+// All other get and put requests
+*/
+
+// Alert when user enters a restricted area
+app.get('/data',function(req,resp){
+
+  // Extract data from the query string
+  msg = req.query.msg;
+  no = "+"+req.query.no.toString();
+  email = req.query.email;
+
+  // Responsr sent backt to main.js
+  resp.send('GET request to the homepage '+msg);
+  console.log(no,msg)
+
+  var mailOptions = {
+    from: 'poulamisarkar101@gmail.com',
+    to: email,
+    subject: 'ALERT!',
+    text: 'Movement detected in restricted area'
+  };
+
+  if (msg=='true'){
+    // Send Whatsapp message (Uncomment next line to send whatsapp message)
+    //message.send_whatsapp('Movement detected in restricted area',no);
+    // Send email
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+  }
+});
+
+//message.bounds.push('[{"lat":12.886877526316793,"lng":77.59036040663413},{"lat":12.88727495285282,"lng":77.59036577105216},{"lat":12.887112844736665,"lng":77.59058034777335}]')
+message.bounds.push('[{"lat":123,"lng":213},{"lat":23,"lng":13}]')
+// Send lat-lng bounds
+app.get('/bounds',function(req,resp){  
+  resp.send(JSON.stringify(message.bounds));
+});
+
